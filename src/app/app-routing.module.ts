70da7dc12b1294f864d32eaa85b6672093a59040
Viniciud <viniciud888@gmail.com>
2020-05-27 10:59:59 -0300

    Rota para dashboard criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { CoachesComponent } from './coaches/coaches.component';
 import { PlayersComponent } from './players/players.component';
 import { ProfileComponent } from './profile/profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
   { path: 'coaches', component: CoachesComponent },
   { path: 'players', component: PlayersComponent },
   { path: 'profile', component: ProfileComponent },
-
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
