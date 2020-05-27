2131e925423444f8851487050a1b71fe3b086300
Viniciud <viniciud888@gmail.com>
2020-05-27 13:22:25 -0300

    exemplos de players e coaches criados

--- src/app/coaches/coaches.component.ts
+++ src/app/coaches/coaches.component.ts
@@ -10,11 +10,11 @@ export class CoachesComponent implements OnInit {
   public title = "Coaches";
 
   public coaches = [
-    { name: 'Zig' },
-    { name: 'Yuuk' },
-    { name: 'SexyCake' },
-    { name: 'Nesk' },
-    { name: 'bulleT' },
+    { id: 1, name: 'Sense', teamGame: 'Raibow Six Siege' },
+    { id: 2, name: 'Yoda', teamGame: 'League of Legends' },
+    { id: 3, name: 'fallen', teamGame: 'Counter Strike: GO' },
+    { id: 4, name: 'lins', teamGame: 'DOTA 2' },
+    { id: 5, name: 'sanss', teamGame: 'Valorant' },
   ];
 
   constructor() { }
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -10,11 +10,11 @@ export class PlayersComponent implements OnInit {
   public title = "Players";
 
   public players = [
-    { name: 'Zig' },
-    { name: 'Yuuk' },
-    { name: 'SexyCake' },
-    { name: 'Nesk' },
-    { name: 'bulleT' },
+    { id: 1, name: 'Zig', mod: 'fps', phone: 7170-1190  },
+    { id: 2, name: 'Yuuk', mod: 'fps', phone: 7270-1490  },
+    { id: 3, name: 'SexyCake', mod: 'fps', phone: 7570-1280  },
+    { id: 4, name: 'Nesk', mod: 'fps', phone: 7075-1291  },
+    { id: 5, name: 'bulleT', mod: 'fps', phone: 7079-1270  },
   ];
 
   constructor() { }
