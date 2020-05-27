a21baf2203439366be479d1e8ab937082119dd17
Viniciud <viniciud888@gmail.com>
2020-05-27 10:44:57 -0300

    Rotas criadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoachesComponent } from './coaches/coaches.component';
+import { PlayersComponent } from './players/players.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'coaches', component: CoachesComponent },
+  { path: 'players', component: PlayersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
