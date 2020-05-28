2e77ca5d93cddfc42e5183eaef77ce65a171a8f4
Viniciud <viniciud888@gmail.com>
2020-05-28 13:43:33 -0300

    Dropdown inserido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TitleComponent } from './title/title.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 @NgModule({
    declarations: [
       AppComponent,
@@ -25,8 +26,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
    imports: [
       BrowserModule,
       AppRoutingModule,
+      BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
-      BrowserAnimationsModule
+
    ],
    providers: [],
    bootstrap: [
